7dba3573b39bfed9048dbdf9d5d4b82a3be415de
d37364214 <d37364214@gmail.com>
2024-08-06 02:56:34 +0000

    header.blade.php

---
+++ resources/views/header.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Soundspot</title>
+                <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+                    <script src="{{ asset('js/script.js') }}" defer></script>
+                    </head>
+                    <body>
+                    <header>
+                        <div class="header-top">
+                                <h1>Soundspot</h1>
+                                        <div class="header-icons">
+                                                    <a href="#" class="icon-bell"></a>
+                                                                <a href="#" class="icon-user"></a>
+                                                                        </div>
+                                                                            </div>
+                                                                                <nav class="main-nav">
+                                                                                        <button class="menu-toggle" id="mobile-menu">MENU</button>
+                                                                                                <ul class="menu">
+                                                                                                            <li>
+                                                                                                                            <a href="#">INSTRUMENTS</a>
+                                                                                                                                            <ul class="submenu">
+                                                                                                                                                                <li><a href="guitare.php">Guitares</a></li>
+                                                                                                                                                                                    <li><a href="piano.php">Pianos</a></li>
+                                                                                                                                                                                                        <li><a href="percussions.php">Percussions</a></li>
+                                                                                                                                                                                                                            <li><a href="prod.php">Production</a></li>
+                                                                                                                                                                                                                                                <li><a href="#">Autres instruments</a></li>
+                                                                                                                                                                                                                                                                </ul>
+                                                                                                                                                                                                                                                                            </li>
+                                                                                                                                                                                                                                                                                        <li><a href="#">NEWS & ACTU</a></li>
+                                                                                                                                                                                                                                                                                                    <li><a href="#">TESTS</a></li>
+                                                                                                                                                                                                                                                                                                                <li><a href="#">ARTICLES</a></li>
+                                                                                                                                                                                                                                                                                                                            <li><a href="#">VIDÉOS</a></li>
+                                                                                                                                                                                                                                                                                                                                        <li><a href="#">AVIS</a></li>
+                                                                                                                                                                                                                                                                                                                                                    <li><a href="#">ANNONCES</a></li>
+                                                                                                                                                                                                                                                                                                                                                                <li><a href="#">FORUMS</a></li>
+                                                                                                                                                                                                                                                                                                                                                                        </ul>
+                                                                                                                                                                                                                                                                                                                                                                            </nav>
+                                                                                                                                                                                                                                                                                                                                                                                <div class="search-bar">
+                                                                                                                                                                                                                                                                                                                                                                                        <input type="text" placeholder="Rechercher sur SoundSpot">
+                                                                                                                                                                                                                                                                                                                                                                                            </div>
+                                                                                                                                                                                                                                                                                                                                                                                            </header>
